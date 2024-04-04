5f7e11921012c454ea8c569e2b04dc68ec7be0b8
Scott Ritchie <smr1204@github.com>
2024-04-04 14:51:36 -0400

    first commit

---
+++ Lab1Ritchie/Laboratory.cpp
@@ -0,0 +1,27 @@
+/*
+ * Program:  <First Laboratory>
+ * Author:   <Scott>
+ * Date:     <1/25/2024>
+ *
+ */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main(void) {
+	
+	float width;
+	float length;
+	float area;
+
+	length = 4.5;
+	width  = 3.35;
+	area   = length * width;
+
+	cout << "Rectangle Area = " << area << "\n";
+
+
+	return 0;
+}
